a6542e945a92bf7e5cbac1cbf29891fe6158283b
badrallehyani <badr.a.allehyani@gmail.com>
2024-04-09 05:06:18 +0300

    removed useless headers

--- Backend - Python(FASTAPI) & Aria2/python files/other_helpers/_1337x.py
+++ Backend - Python(FASTAPI) & Aria2/python files/other_helpers/_1337x.py
@@ -11,7 +11,7 @@ class _1337x:
     def search(keyword: str, page: int = 1):
         keyword = cleanKeyword(keyword)
         searchURL = _1337x.baseURL + f"/search/{keyword}/{str(page)}/"
-        response = get(searchURL, headers={'Accept': '', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7': '', 'Accept-Encoding': '', 'gzip, deflate, br, zstd': '', 'Accept-Language': '', 'en-GB,en;q=0.9,ar;q=0.8,en-US;q=0.7': '', 'Cache-Control': '', 'max-age=0': '', 'Cookie': '', 'cf_clearance=gX3uxYh0L5e_7YT4HyNLer.PCT2Tp2LFytnhj449cdM-1712603200-1.0.1.1-s75hDnAwT.RuSfB4R2_ANgC6IAZToqrw15ln0ofHm98FONbb5Mm585WbjsMpUHszNb_d4ncRNLrI8BMwL9bnbA': '', 'Sec-Ch-Ua': '', '"Microsoft Edge";v="123", "Not': 'A-Brand";v="8", "Chromium";v="123"', 'Sec-Ch-Ua-Mobile': '', '?0': '', 'Sec-Ch-Ua-Platform': '', '"Windows"': '', 'Sec-Fetch-Dest': '', 'document': '', 'Sec-Fetch-Mode': '', 'navigate': '', 'Sec-Fetch-Site': '', 'none': '', 'Sec-Fetch-User': '', '?1': '', 'Upgrade-Insecure-Requests': '', '1': '', 'User-Agent': '', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0': ''})
+        response = get(searchURL, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0'})
         return _1337x.parseSearchResponse(response)
     
     def parseSearchResponse(response: Response):
